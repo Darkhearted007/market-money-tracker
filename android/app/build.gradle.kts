12b6716fd8d796996dff7f078a222703225b9072
Olugbenga Ajayi <richdiamonds@proton.me>
2026-08-15 05:06:38 -0800

    Configure My Money Book Android app

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.osanvaultafrica.mymoneybook"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.osanvaultafrica.mymoneybook"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+}
